b25ac768f064815333915d9c29b16ac04c6442ba
Eduardo Henrique <eduhenriquesrs@gmail.com>
2025-12-31 22:32:02 -0300

    Projeto iniciado

---
+++ src/test/java/com/eduardoh03/IdP/IdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoh03.IdP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
