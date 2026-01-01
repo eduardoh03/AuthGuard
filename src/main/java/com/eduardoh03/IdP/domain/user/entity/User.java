f13027b25defe9dcf6c5d612fb25fa3fba885875
Eduardo Henrique <eduhenriquesrs@gmail.com>
2025-12-31 22:57:20 -0300

    feat: introduzir entidade de usuário e enumeração de função

---
+++ src/main/java/com/eduardoh03/IdP/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.eduardoh03.IdP.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import com.eduardoh03.IdP.domain.user.enums.Role;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/eduardoh03/IdP/domain/user/enums/Role.java
@@ -0,0 +1,6 @@
+package com.eduardoh03.IdP.domain.user.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
